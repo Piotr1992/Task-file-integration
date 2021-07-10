7f499ab313c0772d64b1963a64c561ca97898e1c
Lenovo <szyperek.piotr@o2.pl>
2021-07-10 22:28:06 +0200

    Task 3 - Modulo Spring Integration 1.3

---
+++ src/main/java/com/kodilla/integration/FileTransformer.java
@@ -0,0 +1,18 @@
+package com.kodilla.integration;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FileTransformer {
+
+    public String transformFile(String fileName) throws IOException {
+
+        StringBuilder result = new StringBuilder();
+
+        return result.toString();
+
+    }
+
+}
